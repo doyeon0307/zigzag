656c94b25488150597845c89a5a9143d4072355f
dodo <eg4rang@naver.com>
2025-08-17 23:32:58 +0900

    Feat: 라이브러리 설치 및 Hilt 설정

---
+++ app/src/main/java/com/dokarun/zigzag/ZigzagApp.kt
@@ -0,0 +1,9 @@
+package com.dokarun.zigzag
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+const val TAG: String = "ZIGZAG"
+
+@HiltAndroidApp
+class ZigzagApp : Application()
